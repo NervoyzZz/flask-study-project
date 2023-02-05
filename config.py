e6245bfb20655c64f6627b83c9daaac922e18e8c
Daniil <nervoidaz@yandex.ru>
2023-02-05 22:58:01 +0300

    Added configs

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+import os
 
+
+class BaseConfig:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'A SECRET KEY'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or \
+        'mysql+pymysql://root:pass@localhost/flask_app_db'
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PRODUCTION_DATABASE_URI')
