de1fc6d24ae41cf3ce8f77bd351da85dab919c01
Daniil <nervoidaz@yandex.ru>
2023-02-06 23:18:19 +0300

    Prepare init file for app directory

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,17 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
