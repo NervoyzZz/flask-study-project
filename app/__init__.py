f42c249d4d92211a405722d753d78adb6c1a87a0
Daniil <nervoidaz@yandex.ru>
2023-02-07 00:06:03 +0300

    Added Users model

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_login import LoginManager()
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
 migrate = Migrate()
-
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
 
 def create_app(config):
     app = Flask(__name__)
