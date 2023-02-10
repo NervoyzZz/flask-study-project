55faa400d458fd4806e6661d160127d9be450a2c
Daniil <nervoidaz@yandex.ru>
2023-02-10 14:47:08 +0300

    Render test index page

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_login import LoginManager()
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,11 +9,15 @@ migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = 'main.login'
 
+
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
