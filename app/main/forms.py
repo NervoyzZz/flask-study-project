ed98d14d5efc8ed381fe35422b8f952e7e2b7be7
Daniil <nervoidaz@yandex.ru>
2023-02-10 16:37:03 +0300

    Added login form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,1 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField()
