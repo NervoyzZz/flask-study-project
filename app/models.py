74e0d7c7d9a5df379f60df1b9db47f13b9043fd7
Daniil <nervoidaz@yandex.ru>
2023-02-19 23:18:05 +0300

    Set salt length to correspond field size

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class User(db.Model, UserMixin):
         return "<{}:{}>".format(self.id, self.username)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password, salt_length=12)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
