f74f4bf7012c5b06068ac829aa0f0e045b21a7bd
Daniil <nervoidaz@yandex.ru>
2023-02-26 22:59:17 +0300

    Use password input for password fields

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email
+from wtforms.widgets import PasswordInput
 
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    password = StringField("Password", widget=PasswordInput(hide_value=False),
+                           validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField()
 
@@ -13,12 +15,14 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     username = StringField("Username", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    password = StringField("Password", widget=PasswordInput(hide_value=False),
+                           validators=[DataRequired()])
     email = StringField("Email", validators=[Email()])
     remember = BooleanField("Remember Me")
     submit = SubmitField()
 
 
 class DeleteForm(FlaskForm):
-    password = StringField("Password", validators=[DataRequired()])
+    password = StringField("Password", widget=PasswordInput(hide_value=False),
+                           validators=[DataRequired()])
     submit = SubmitField()
