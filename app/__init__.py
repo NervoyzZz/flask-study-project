a34eae9cd11e0fafb4548ec0572462276da9f0b2
Daniil <nervoidaz@yandex.ru>
2023-02-27 00:01:09 +0300

    Added 404 handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -10,8 +10,13 @@ login_manager = LoginManager()
 login_manager.login_view = 'main.login'
 
 
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 def create_app(config):
     app = Flask(__name__)
+    app.register_error_handler(404, page_not_found)
     app.config.from_object(config)
 
     db.init_app(app)
